a46f7568e9ec33b575db7da5e1a157c177848a98
Shaj2311 <haideralijafri2007@gmail.com>
2026-09-03 12:11:19 +0500

    Create basic struct for audio channel 2

---
+++ include/apu.h
@@ -0,0 +1,13 @@
+#ifndef APU_H
+#define APU_H
+#include <stdint.h>
+
+typedef struct
+{
+	uint16_t frequencyTimer;	//down counter
+	uint16_t reloadFrequency;	//reset to this when timer hits zero
+	uint8_t dutyPattern;		//50%, 25% etc.
+	uint8_t dutyIndex;		//current position in duty pattern
+} CH2;
+
+#endif
